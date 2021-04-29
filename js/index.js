76e04161d2f04dc568d9bf255848f971f29768ac
rodrigo debian <correo.rodrigo@gmail.com>
2021-04-28 19:31:40 -0500

    saving

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ const apiPath = "http://localhost:8000/";
 let api = {
     get: async (url) => {
         return await $.get(`${apiPath}${url}`);
+    },
+
+    post: async (url, data) => {
+        return await $.ajax({
+            type: "POST",
+            url: `${apiPath}${url}`,
+            data: data,
+            dataType: "json",
+            contentType: "multipart/form-data"
+          });
     }
 }
 
@@ -14,12 +24,51 @@ $(async () => {
         Actions: "Actions",
         NewPayment: "NewPayment",
     }
+    let newPayment = () => {
+        return {
+            id: ko.observable(0),
+            creditId: ko.observable(creditId),                    
+            number: ko.observable(0),
+            isPaymentOnTime: ko.observable(true),
+            isViaPayrol: ko.observable(true),
+            documentId: ko.observable(),
+            comments: ko.observable(),
+            paymentDate: ko.observable(),
+            amount: ko.observable()
+        };            
+    };
+
+    let mapPayments = (payments) => {            
+        let arr = [];
+        return payments.map(p => {
+            let it = newPayment();
+            it.id(p.id);
+            it.creditId(p.creditId);
+            it.number(p.number);
+            it.isPaymentOnTime(p.isPaymentOnTime);
+            it.isViaPayrol(p.isViaPayrol);
+            it.documentId(p.documentId);
+            it.comments(p.comments);
+            it.paymentDate(p.paymentDate);
+            it.amount(p.amount);
+            return it;
+        });        
+    };
 
     let model = {
         mode: ko.observable(modes.Actions),
         credit,
-        payments
+        payments: ko.observableArray(),
+        editingPayment: ko.observable(),        
+        savePayment: async (payment) => {
+            //ko.toJSON
+            let data = ko.toJS(payment);
+            await api.post("payment", data);
+        }
     };
+
+    model.payments(mapPayments(payments));
+    model.editingPayment(newPayment());
 
     ko.applyBindings(model, document.getElementById("main"));
 });
